78c1751600cd8ff2ebc5b5f7f3b330741722fed3
DamienLyon <renou.damien@gmail.com>
2022-01-21 10:07:13 +0100

    Add files via upload

---
+++ editor/inc/head.inc.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8" />
+
+<title>Editor Studio</title>
+
+<link rel="icon" type="image/png" sizes="192x192"  href="../img/base/newfav/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../img/base/newfav/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../img/base/newfav/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../img/base/newfav/favicon-16x16.png">
+
+<meta name="theme-color" content="#ffffff">
+
+<script src="dist/js/filestack-0.1.10.js"></script>
+<script src="dist/js/grapesludi.js"></script>
+<script src="dist/grapesjs-preset-webpage.min.js"></script>
+<script src="jscss/jquery.js"></script>
+<script src="jscss/amplify.min.js"></script>
